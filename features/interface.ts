3763ee77a2d074269199429639a49e7128bbd7a8
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-06-01 12:29:15 +0500

    interface

--- features/interface.ts
+++ features/interface.ts
@@ -1,14 +1,16 @@
+interface Public {
+    name: string,
+    color: string,
+    wheels: number
+}
+
 const cars = {
     name: 'BMW',
     color: 'red',
     wheels: 4
 }
 
-const myCar = (vehicles: {
-    name: string,
-    color: string,
-    wheels: number
-}): void => {
+const myCar = (vehicles: Public ): void => {
     console.log(`
         My car ${vehicles.name},
         color is ${vehicles.color},
